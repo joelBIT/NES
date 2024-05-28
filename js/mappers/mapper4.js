22131b2983acf17837f600aaa95d34f2f9cfa52e
joelBit <joel.rollny@gmail.com>
2024-05-28 17:39:29 +0200

    Comments on mapper 4

--- js/mappers/mapper4.js
+++ js/mappers/mapper4.js
@@ -1,6 +1,30 @@
 import { Mirror } from "../mirror.js";
 import { Mapper } from "./mapper.js";
 
+
+class BankRegister {
+  register = new Uint32Array(8);
+
+  getRegisterData(register) {
+    return this.register[register];
+  }
+
+  /**
+   * R6 and R7 will ignore the top two bits, as the MMC3 has only 6 PRG ROM address lines.
+   * R0 and R1 ignore the bottom bit, as the value written still counts banks in 1KB units but odd numbered banks can't be selected.
+   */
+  setData(register, data) {
+    if (register === 6 || register === 7) {
+      this.register[register] = data & 0x3F;
+    } else if (register === 0 || register === 1) {
+      this.register[register] = data & 0xFE;
+    } else {
+      this.register[register] = data;
+    }
+  }
+}
+
+
 /**
  * Mapper 4. The Nintendo MMC3 is a mapper ASIC used in Nintendo's TxROM Game Pak boards. Most common TxROM boards,
  * along with the NES-HKROM board (which uses the Nintendo MMC6), are assigned to iNES Mapper 004.
@@ -33,19 +57,22 @@ export class MapperFour extends Mapper {
   VRAM = new Uint8Array(32 * 1024);
   mirrorMode = Mirror.HORIZONTAL;
 
-  targetRegister = new Uint8Array(1);
+  ONE_KILOBYTE = 0x0400;
+  EIGHT_KILOBYTE = 0x2000;
+
+  bankRegister = new BankRegister();
+
+  targetBankRegister = new Uint8Array(1);
   programBankMode = false;
   characterInversion = false;
 
-  register = new Uint32Array(8);
   characterBank = new Uint32Array(8);
   programBank = new Uint32Array(4);
 
   irqActive = false;
-  irqEnable = false;
-  irqUpdate = false;
+  irqEnabled = false;
   irqCounter = new Uint16Array(1);
-  irqReload = new Uint16Array(1);
+  irqLatch = new Uint16Array(1);
 
   constructor(programBanks, characterBanks) {
     super(programBanks, characterBanks);
@@ -79,61 +106,68 @@ export class MapperFour extends Mapper {
     return false;
   }
 
+  /**
+   * The MMC3 has 4 pairs of registers at $8000-$9FFF, $A000-$BFFF, $C000-$DFFF, and $E000-$FFFF - even
+   * addresses ($8000, $8002, etc.) select the low register and odd addresses ($8001, $8003, etc.) select the
+   * high register in each pair. These can be broken into two independent functional units: memory mapping
+   * ($8000, $8001, $A000, $A001) and scanline counting ($C000, $C001, $E000, $E001).
+   *
+   * Bank select ($8000-$9FFE, even)
+   * 7  bit  0
+   * ---- ----
+   * CPMx xRRR
+   * |||   |||
+   * |||   +++- Specify which bank register to update on next write to Bank Data register
+   * |||          000: R0: Select 2 KB CHR bank at PPU $0000-$07FF (or $1000-$17FF)
+   * |||          001: R1: Select 2 KB CHR bank at PPU $0800-$0FFF (or $1800-$1FFF)
+   * |||          010: R2: Select 1 KB CHR bank at PPU $1000-$13FF (or $0000-$03FF)
+   * |||          011: R3: Select 1 KB CHR bank at PPU $1400-$17FF (or $0400-$07FF)
+   * |||          100: R4: Select 1 KB CHR bank at PPU $1800-$1BFF (or $0800-$0BFF)
+   * |||          101: R5: Select 1 KB CHR bank at PPU $1C00-$1FFF (or $0C00-$0FFF)
+   * |||          110: R6: Select 8 KB PRG ROM bank at $8000-$9FFF (or $C000-$DFFF)
+   * |||          111: R7: Select 8 KB PRG ROM bank at $A000-$BFFF
+   * ||+------- Nothing on the MMC3, see MMC6
+   * |+-------- PRG ROM bank mode (0: $8000-$9FFF swappable,
+   * |                                $C000-$DFFF fixed to second-last bank;
+   * |                             1: $C000-$DFFF swappable,
+   * |                                $8000-$9FFF fixed to second-last bank)
+   * +--------- CHR A12 inversion (0: two 2 KB banks at $0000-$0FFF, four 1 KB banks at $1000-$1FFF;
+   *                               1: two 2 KB banks at $1000-$1FFF, four 1 KB banks at $0000-$0FFF)
+   *
+   */
   mapWriteCPU(address, data) {
     if (address >= 0x6000 && address <= 0x7FFF) {
       this.VRAM[address & 0x1FFF] = data;
       return { "address": 0xFFFFFFFF };
     }
 
-    if (address >= 0x8000 && address <= 0x9FFF) {
-      // Bank Select
-      if (!(address & 0x0001)) {
-        this.targetRegister[0] = data & 0x07;
+    const isEven = address % 2 === 0;
+
+    if (address >= 0x8000 && address <= 0x9FFF) {   // CPU $8000-$9FFF (or $C000-$DFFF): 8 KB switchable PRG ROM bank
+      if (isEven) {   // Bank Select
+        this.targetBankRegister[0] = data & 0x07;
         this.programBankMode = (data & 0x40) > 0;
         this.characterInversion = (data & 0x80) > 0;
-      } else {
-        // Update target register
-        this.register[this.targetRegister[0]] = data;
 
-        // Update Pointer Table
-        if (this.characterInversion) {
-          this.characterBank[0] = this.register[2] * 0x0400;
-          this.characterBank[1] = this.register[3] * 0x0400;
-          this.characterBank[2] = this.register[4] * 0x0400;
-          this.characterBank[3] = this.register[5] * 0x0400;
-          this.characterBank[4] = (this.register[0] & 0xFE) * 0x0400;
-          this.characterBank[5] = this.register[0] * 0x0400 + 0x0400;
-          this.characterBank[6] = (this.register[1] & 0xFE) * 0x0400;
-          this.characterBank[7] = this.register[1] * 0x0400 + 0x0400;
-        } else {
-          this.characterBank[0] = (this.register[0] & 0xFE) * 0x0400;
-          this.characterBank[1] = this.register[0] * 0x0400 + 0x0400;
-          this.characterBank[2] = (this.register[1] & 0xFE) * 0x0400;
-          this.characterBank[3] = this.register[1] * 0x0400 + 0x0400;
-          this.characterBank[4] = this.register[2] * 0x0400;
-          this.characterBank[5] = this.register[3] * 0x0400;
-          this.characterBank[6] = this.register[4] * 0x0400;
-          this.characterBank[7] = this.register[5] * 0x0400;
-        }
-
-        if (this.programBankMode) {
-          this.programBank[2] = (this.register[6] & 0x3F) * 0x2000;
-          this.programBank[0] = (this.programBanks * 2 - 2) * 0x2000;
-        } else {
-          this.programBank[0] = (this.register[6] & 0x3F) * 0x2000;
-          this.programBank[2] = (this.programBanks * 2 - 2) * 0x2000;
-        }
-
-        this.programBank[1] = (this.register[7] & 0x3F) * 0x2000;
-        this.programBank[3] = (this.programBanks * 2 - 1) * 0x2000;
-
+        /**
+         * Bank data ($8001-$9FFF, odd)
+         * 7  bit  0
+         * ---- ----
+         * DDDD DDDD
+         * |||| ||||
+         * ++++-++++- New bank value, based on last value written to Bank select register
+         */
+      } else {    // Bank Data
+        this.bankRegister.setData(this.targetBankRegister[0], data);
+        this.updateCharacterBanks();
+        this.updateProgramBanks();
       }
 
       return false;
     }
 
     if (address >= 0xA000 && address <= 0xBFFF) {
-      if (!(address & 0x0001)) {
+      if (isEven) {
         if (data & 0x01) {
           this.mirrorMode = Mirror.HORIZONTAL;
         } else {
@@ -143,30 +177,35 @@ export class MapperFour extends Mapper {
       return false;
     }
 
+    /**
+     * This latch specifies the IRQ counter reload value. When the IRQ counter is zero (or a reload is requested
+     * through $C001), this value will be copied to the IRQ counter at the NEXT rising edge of the PPU address,
+     * presumably at PPU cycle 260 of the current scanline.
+     */
     if (address >= 0xC000 && address <= 0xDFFF) {
-      if (!(address & 0x0001)) {
-        this.irqReload[0] = data;
+      if (isEven) {
+        this.irqLatch[0] = data;
       } else {
-        this.irqCounter[0] = 0x0000;
+        this.irqCounter[0] = 0x0000;    // Writing any value to this register clears the MMC3 IRQ counter immediately
       }
       return false;
     }
 
+
     if (address >= 0xE000 && address <= 0xFFFF) {
-      if (!(address & 0x0001)) {
-        this.irqEnable = false;
+      if (isEven) {
+        this.irqEnabled = false;
         this.irqActive = false;
       } else {
-        this.irqEnable = true;
+        this.irqEnabled = true;
       }
-      return false;
     }
 
     return false;
   }
 
   mapReadPPU(address) {
-    if (address >= 0x0000 && address <= 0x03FF) {
+    if (address < 0x0400) {
       return { "address": this.characterBank[0] + (address & 0x03FF) };
     }
 
@@ -206,16 +245,17 @@ export class MapperFour extends Mapper {
   }
 
   reset() {
-    this.targetRegister[0] = 0x00;
+    this.targetBankRegister[0] = 0x00;
     this.programBankMode = false;
     this.characterInversion = false;
     this.mirrorMode = Mirror.HORIZONTAL;
 
+    this.bankRegister = new BankRegister();
+
     this.irqActive = false;
-    this.irqEnable = false;
-    this.irqUpdate = false;
+    this.irqEnabled = false;
     this.irqCounter[0] = 0x0000;
-    this.irqReload[0] = 0x0000;
+    this.irqLatch[0] = 0x0000;
 
     for (let i = 0; i < this.programBank.length; i++) {
       this.programBank[i] = 0;
@@ -223,13 +263,12 @@ export class MapperFour extends Mapper {
 
     for (let i = 0; i < this.characterBank.length; i++) {
       this.characterBank[i] = 0;
-      this.register[i] = 0;
     }
 
     this.programBank[0] = 0;
-    this.programBank[1] = 0x2000;
-    this.programBank[2] = (this.programBanks * 2 - 2) * 0x2000;
-    this.programBank[3] = (this.programBanks * 2 - 1) * 0x2000;
+    this.programBank[1] = this.EIGHT_KILOBYTE;
+    this.programBank[2] = (this.programBanks * 2 - 2) * this.EIGHT_KILOBYTE;
+    this.programBank[3] = (this.programBanks * 2 - 1) * this.EIGHT_KILOBYTE;
   }
 
   irqState() {
@@ -242,14 +281,81 @@ export class MapperFour extends Mapper {
 
   scanLine() {
     if (this.irqCounter[0] === 0) {
-      this.irqCounter[0] = this.irqReload[0];
+      this.irqCounter[0] = this.irqLatch[0];
     } else {
       this.irqCounter[0]--;
     }
 
-    if (this.irqCounter[0] === 0 && this.irqEnable) {
+    if (this.irqCounter[0] === 0 && this.irqEnabled) {
       this.irqActive = true;
     }
+  }
+
+  /**
+   *
+   * CHR map mode →	$8000.D7 = 0	$8000.D7 = 1
+   * PPU Bank	Value of MMC3 register
+   * $0000-$03FF	    R0	          R2
+   * $0400-$07FF	                  R3
+   * $0800-$0BFF	    R1	          R4
+   * $0C00-$0FFF	                  R5
+   * $1000-$13FF	    R2	          R0
+   * $1400-$17FF	    R3
+   * $1800-$1BFF	    R4	          R1
+   * $1C00-$1FFF	    R5
+   *
+   * 2KB banks may only select even numbered CHR banks. (The lowest bit is ignored.)
+   */
+  updateCharacterBanks() {
+    if (this.characterInversion) {
+      this.characterBank[0] = this.bankRegister.getRegisterData(2) * this.ONE_KILOBYTE;
+      this.characterBank[1] = this.bankRegister.getRegisterData(3) * this.ONE_KILOBYTE;
+      this.characterBank[2] = this.bankRegister.getRegisterData(4) * this.ONE_KILOBYTE;
+      this.characterBank[3] = this.bankRegister.getRegisterData(5) * this.ONE_KILOBYTE;
+      this.characterBank[4] = this.bankRegister.getRegisterData(0) * this.ONE_KILOBYTE;
+      this.characterBank[5] = this.bankRegister.getRegisterData(0) * this.ONE_KILOBYTE + this.ONE_KILOBYTE;
+      this.characterBank[6] = this.bankRegister.getRegisterData(1) * this.ONE_KILOBYTE;
+      this.characterBank[7] = this.bankRegister.getRegisterData(1) * this.ONE_KILOBYTE + this.ONE_KILOBYTE;
+    } else {
+      this.characterBank[0] = this.bankRegister.getRegisterData(0) * this.ONE_KILOBYTE;
+      this.characterBank[1] = this.bankRegister.getRegisterData(0) * this.ONE_KILOBYTE + this.ONE_KILOBYTE;
+      this.characterBank[2] = this.bankRegister.getRegisterData(1) * this.ONE_KILOBYTE;
+      this.characterBank[3] = this.bankRegister.getRegisterData(1) * this.ONE_KILOBYTE + this.ONE_KILOBYTE;
+      this.characterBank[4] = this.bankRegister.getRegisterData(2) * this.ONE_KILOBYTE;
+      this.characterBank[5] = this.bankRegister.getRegisterData(3) * this.ONE_KILOBYTE;
+      this.characterBank[6] = this.bankRegister.getRegisterData(4) * this.ONE_KILOBYTE;
+      this.characterBank[7] = this.bankRegister.getRegisterData(5) * this.ONE_KILOBYTE;
+    }
+  }
+
+  /**
+   * Bit 6 of the last value written to $8000 swaps the PRG windows at $8000 and $C000. The MMC3 uses one map if
+   * bit 6 was cleared to 0 (value & $40 == $00) and another if set to 1 (value & $40 == $40).
+   *
+   * PRG map mode →	$8000.D6 = 0	$8000.D6 = 1
+   * CPU Bank	Value of MMC3 register
+   * $8000-$9FFF	      R6	          (-2)
+   * $A000-$BFFF	      R7	          R7
+   * $C000-$DFFF	      (-2)	        R6
+   * $E000-$FFFF	      (-1)	        (-1)
+   *
+   * (-1) : the last bank
+   * (-2) : the second last bank
+   *
+   * Because the values in R6, R7, and $8000 are unspecified at power on, the reset vector must point into $E000-$FFFF,
+   * and code must initialize these before jumping out of $E000-$FFFF.
+   */
+  updateProgramBanks() {
+    if (this.programBankMode) {
+      this.programBank[2] = this.bankRegister.getRegisterData(6) * this.EIGHT_KILOBYTE;
+      this.programBank[0] = (this.programBanks * 2 - 2) * this.EIGHT_KILOBYTE;
+    } else {
+      this.programBank[0] = this.bankRegister.getRegisterData(6) * this.EIGHT_KILOBYTE;
+      this.programBank[2] = (this.programBanks * 2 - 2) * this.EIGHT_KILOBYTE;
+    }
+
+    this.programBank[1] = this.bankRegister.getRegisterData(7) * this.EIGHT_KILOBYTE;
+    this.programBank[3] = (this.programBanks * 2 - 1) * this.EIGHT_KILOBYTE;
   }
 
   mirror() {
